a1ae657cfa78a19292c43f351435d8dc97f347ff
sazonovitas <itassudo@gmail.com>
2024-11-19 05:01:20 -0500

    deploy: adding monitoring for concourse ci

--- scripts/create-k3s-secrets.sh
+++ scripts/create-k3s-secrets.sh
@@ -4,21 +4,21 @@ K3S_NAMESPACE=concourse
 CONCOURSE_WEB_KEYS=concourse-web-keys
 kubectl get -n $K3S_NAMESPACE secret $CONCOURSE_WEB_KEYS &> /dev/null || \
 	kubectl create -n $K3S_NAMESPACE secret generic $CONCOURSE_WEB_KEYS \
-	--from-file=./keys/web/authorized_worker_keys \
-	--from-file=./keys/web/session_signing_key \
-	--from-file=./keys/web/session_signing_key.pub \
-	--from-file=./keys/web/tsa_host_key \
-	--from-file=./keys/web/tsa_host_key.pub
+	--from-file=keys/web/authorized_worker_keys \
+	--from-file=keys/web/session_signing_key \
+	--from-file=keys/web/session_signing_key.pub \
+	--from-file=keys/web/tsa_host_key \
+	--from-file=keys/web/tsa_host_key.pub
 
 CONCOURSE_WORKER_KEYS=concourse-worker-keys
 kubectl get -n $K3S_NAMESPACE secret $CONCOURSE_WORKER_KEYS &> /dev/null || \
 	kubectl create -n $K3S_NAMESPACE secret generic $CONCOURSE_WORKER_KEYS \
-	--from-file=./keys/worker/tsa_host_key.pub \
-	--from-file=./keys/worker/worker_key \
-	--from-file=./keys/worker/worker_key.pub
+	--from-file=keys/worker/tsa_host_key.pub \
+	--from-file=keys/worker/worker_key \
+	--from-file=keys/worker/worker_key.pub
 
 CONCOURSE_SSL_CERTS=concourse-ssl-certs
 kubectl get -n $K3S_NAMESPACE secret $CONCOURSE_SSL_CERTS &> /dev/null || \
 	kubectl create -n $K3S_NAMESPACE secret generic $CONCOURSE_SSL_CERTS \
-	--from-file=./certs/server.key \
-	--from-file=./certs/server.crt
+	--from-file=certs/server.key \
+	--from-file=certs/server.crt
